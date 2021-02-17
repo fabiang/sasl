e6bdcdb2a000f1046fc6eae7cba4ac6252336c1f
Fabian Grutschus <f.grutschus@lubyte.de>
2021-02-17 15:01:30 +0100

    Upload test artifacts only when running 7.4

--- tests/upload_artefacts.sh
+++ tests/upload_artefacts.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 set +x
-wget https://scrutinizer-ci.com/ocular.phar
-php ocular.phar code-coverage:upload --format=php-clover build/logs/clover.xml
-php vendor/bin/coveralls
-php vendor/bin/test-reporter --stdout > codeclimate.json
-curl -X POST -d @codeclimate.json -H 'Content-Type: application/json' \
-     -H 'User-Agent: Code Climate (PHP Test Reporter v0.1.1)' \
-     https://codeclimate.com/test_reports
+
+if [[ -n "$UPLOAD_ARTIFACTS" && "$UPLOAD_ARTIFACTS" -eq 1 ]]; then
+    wget https://scrutinizer-ci.com/ocular.phar
+    php ocular.phar code-coverage:upload --format=php-clover build/logs/clover.xml
+    php vendor/bin/coveralls
+    php vendor/bin/test-reporter --stdout > codeclimate.json
+    curl -X POST -d @codeclimate.json -H 'Content-Type: application/json' \
+         -H 'User-Agent: Code Climate (PHP Test Reporter v0.1.1)' \
+         https://codeclimate.com/test_reports
+fi
