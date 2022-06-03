56b0a6bf6eef5cb3f774f1fcf3a02848b1303226
David Molnar <dmolnar@qualityunit.com>
2022-06-03 16:27:24 +0200

    Add missing methods to the Throwable polyfill
    To prevent issues with static analysis.

--- src/throwable.php
+++ src/throwable.php
@@ -2,5 +2,23 @@
 // @codingStandardsIgnoreFile
 // phpcs:ignore
 if (!interface_exists('Throwable')) {
-    interface Throwable {}
+    interface Throwable
+    {
+        /** @return string */
+        public function getMessage();
+        /** @return int */
+        public function getCode();
+        /** @return string */
+        public function getFile();
+        /** @return int */
+        public function getLine();
+        /** @return array */
+        public function getTrace();
+        /** @return string */
+        public function getTraceAsString();
+        /** @return null|Throwable */
+        public function getPrevious();
+        /** @return string */
+        public function __toString();
+    }
 }
